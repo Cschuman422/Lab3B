bd588d6947aa614670eab9c51501ff92ba5edfca
Cschuman422 <63675306+Cschuman422@users.noreply.github.com>
2020-04-27 22:38:20 -0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Alcohol_App
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void Btn_Clicked(object sender, System.EventArgs e)
+        {
+            int age = Int32.Parse(Inp_age.Text);
+            if(age > 21)
+            {
+                Lbl_BMI.Text = "You can buy alcohol";
+            }
+            else if (age < 21)
+            {
+                int years = 21 - age;
+                Lbl_BMI.Text = $"You can buy alcohol in {years} years";
+            }
+           
+            
+        }
+    }
+}
